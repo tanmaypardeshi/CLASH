95e2b07a5349667249f3b8bf3be649a67eeae70e
Tanmay Pardeshi <53749887+tanmaypardeshi@users.noreply.github.com>
2020-01-08 09:53:53 +0530

    Update urls.py

--- RC/problem3/urls.py
+++ RC/problem3/urls.py
@@ -2,6 +2,6 @@ from django.contrib import admin
 from django.urls import path,include
 
 urlpatterns = [
-    path('',include('project.urls')),
     path('admin/', admin.site.urls),
+    path('',include('project.urls')),
 ]

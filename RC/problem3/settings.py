b1125bf5d48510385697474db5ec7993c69490b4
tanmaypardeshi <tanmaypardeshi@gmail.com>
2020-03-21 16:37:05 +0530

    Added views

--- RC/problem3/settings.py
+++ RC/problem3/settings.py
@@ -12,24 +12,14 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 
 import os
 
-# Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 STATIC_DIR = os.path.join(BASE_DIR, "project/static")
 
-
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'ja=epv7t5!8fb_e(fj=xff@d2jq1*_mjco^l&0g4c7nq!3nfrf'
 
-# SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
 ALLOWED_HOSTS = ['*']
-
-
-# Application definition
 
 INSTALLED_APPS = [
     'project',
